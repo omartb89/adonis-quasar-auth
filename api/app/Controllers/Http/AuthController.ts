dee3d5bdaa0c87ebdee4caa83a667a72cd018e32
omartb89 <68015377+omartb89@users.noreply.github.com>
2022-01-14 18:10:49 -0500

    separated backend and front

---
+++ api/app/Controllers/Http/AuthController.ts
@@ -0,0 +1,41 @@
+import {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import {rules, schema} from '@ioc:Adonis/Core/Validator'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+export default class AuthController {
+  public async index () {
+    return User.all()
+  }
+
+  public async register ({ request, response }: HttpContextContract) {
+    const validations = await schema.create({
+      email: await schema.string({}, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email'})
+      ]),
+      password: await schema.string({}, [
+        rules.confirmed()
+      ])
+    })
+    const data = await request.validate({schema: validations})
+    const user = await User.create({...data})
+    return response.created(user)
+  }
+
+  public async login ({ request, response, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+    const user = await User
+      .query()
+      .where('email', email)
+      .firstOrFail()
+    /* const token = await auth.use('api').attempt(email, password) */
+    if (!(await Hash.verify(user.password, password))) {
+      return response.badRequest('Invalid credentials')
+    } else {
+      const token = await auth.use('api').generate(user)
+      return token.toJSON()
+    }
+  }
+}
---
+++ front/README.md
@@ -0,0 +1,26 @@
+# Coinmaster (coinmaster)
+
+The app may track your income, help you with money investment, display info in cool grafics and other stuffs
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
